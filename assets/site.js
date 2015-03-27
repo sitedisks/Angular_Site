addcbeed24bf18f1ba091d55a521312be69943b4
Peter <peter@sitedisks.com.au>
2015-03-28 09:35:13 +1100

    AngularJS package

--- assets/site.js
+++ assets/site.js
@@ -1,6 +1,14 @@
 ﻿
 var siteApp = angular.module('siteApp', ['ui.bootstrap']);
-siteApp.controller('testCtrl', function ($scope) {
-    $scope.message = 'message from Angular';
+siteApp.controller('testCtrl', function ($scope, $http) {
+    $scope.message = 'Message from Angular';
+    $scope.loadData = loadData;
+
+    function loadData() {
+        $http.get("dummyData.json").success(function (data) {
+            $scope.products = data;
+        });
+    };
+
 });
 
